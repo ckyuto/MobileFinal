ddf75b594b67f9b69f1a728feaf3682e31f98a8d
Mengzhen <hmz@mengzhendembp.wv.cc.cmu.edu>
2016-07-19 14:51:11 -0400

    QAQ

---
+++ MobileFinaliOS/NewClass.m
@@ -0,0 +1,48 @@
+//
+//  NewClass.m
+//  MobileFinaliOS
+//
+//  Created by Mengzhen on 7/19/16.
+//  Copyright © 2016 Carnegie Mellon University. All rights reserved.
+//
+
+#import "NewClass.h"
+
+@interface NewClass ()
+
+@end
+
+@implementation NewClass
+
+@synthesize courseName;
+@synthesize description;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)saveClass:(id)sender {
+    NSString * name = courseName.text;
+    NSString * desc = description.text;
+    NSLog(@"%@" @"%@", name, desc);
+}
+
+- (IBAction)cancelClass:(id)sender {
+}
+@end
